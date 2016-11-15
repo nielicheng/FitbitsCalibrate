43bf7ab6719cb3905e1b84d6b3959b568780110d
Licheng Nie <nielicheng@gmail.com>
2016-11-15 21:53:30 +1100

    Add test case of instruction parse

---
+++ src/test/java/com/nie/fitbits/InstructionParserTest.java
@@ -0,0 +1,20 @@
+package com.nie.fitbits;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InstructionParserTest {
+
+	@Test
+	public void whenAStringIsTwoPositiveNumbersSeparatedBySpaceThenAPitchInstanceIsReturned() {
+		String firstLine = "5 4";
+		InstructionParser parser = new InstructionParser();
+		PitchSize pitchSize = parser.parsePitchSize(firstLine);
+		int expectedGridNumX = 5;
+		int expectedGridNumY = 4;
+		assertEquals(expectedGridNumX, pitchSize.getGridNumX());
+		assertEquals(expectedGridNumY, pitchSize.getGridNumY());
+	}
+
+}
