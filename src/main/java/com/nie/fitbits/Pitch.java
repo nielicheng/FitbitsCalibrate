822d269005f78d353cfcc481118527dc6cd0e4d3
Licheng Nie <nielicheng@gmail.com>
2016-11-15 20:09:56 +1100

    Add Pitch implementation

---
+++ src/main/java/com/nie/fitbits/Pitch.java
@@ -0,0 +1,31 @@
+package com.nie.fitbits;
+
+public class Pitch {
+
+	private int lower = 0;
+	private int left = 0;
+	private int upper;
+	private int right;
+	
+	public Pitch(int gridNumX, int gridNumY) {
+		this.upper = gridNumY - 1;
+		this.right = gridNumX - 1;
+	}
+
+	public int getLower() {
+		return lower;
+	}
+
+	public int getLeft() {
+		return left;
+	}
+
+	public int getUpper() {
+		return upper;
+	}
+
+	public int getRight() {
+		return right;
+	}
+
+}
